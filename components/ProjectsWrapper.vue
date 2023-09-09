f6265343c1799f6a36d571c2d811461b4b9eee68
colin casazza <colanzio5@gmail.com>
2023-09-09 16:37:54 -0700

    links notebooks to external site

--- components/ProjectsWrapper.vue
+++ components/ProjectsWrapper.vue
@@ -2,13 +2,13 @@
   <ClientOnly>
     <div v-for="category in categories" :key="category?.subject" class="flex flex-col text-sm">
       <div v-for="item in category?.items" :key="item.heading" class="mb-2">
-        <NuxtLink class="link" :to="item.link">
+        <NuxtLink class="link" :to="item.link" :target="item.isLocal ? '' : '_blank'">
           {{
             getFullLink(item)
           }}
         </NuxtLink>
         <br>
-        <span class="text" v-html="item.text" />
+        <span class="text">{{ item.text }}</span>
       </div>
     </div>
   </ClientOnly>
@@ -17,8 +17,9 @@
 <script setup lang="ts">
 
 const host = computed(() => window.location.host)
-function getFullLink (item: { link: string }): string {
-  return (host.value ?? 'info') + item.link
+
+function getFullLink (item: { link: string, isLocal: boolean } = { link: '', isLocal: false }): string {
+  return item.isLocal ? ((host.value ?? 'info') + item.link) : item.link
 }
 
 // ! this (and router objects) should both be coming
@@ -29,8 +30,9 @@ const categories = [
     items: [
       {
         heading: 'Flocking',
-        text: 'Simulated flocking behavior based on separation, alignment, and cohesion. Implemented w/ rust/wasm, inspired by <a class=\'link\' href=\'https://thecodingtrain.com/CodingChallenges/124-flocking-boids.html\'>thecodingtrain.com<a/>.',
-        link: '/projects/flock'
+        text: 'Simulated flocking behavior based on separation, alignment, and cohesion. ',
+        link: '/projects/flock',
+        isLocal: true
       }
     ]
   },
@@ -38,14 +40,16 @@ const categories = [
     subject: 'notebooks',
     items: [
       {
+        isLocal: false,
         heading: 'Kinematics',
-        text: 'Relatively in depth analysis of the kinematics of robotic manipulator.',
-        link: '/projects/notebooks/kinematics'
+        text: 'An analysis of the kinematics of robotic manipulator.',
+        link: 'https://olivecasazza.github.io/notebooks/lab?path=SDSU-CS556-Workspace/a4/p4.ipynb'
       },
       {
+        isLocal: false,
         heading: 'Inverse Kinematics Approximation',
         text: 'Approximation of a 2d inverse kinematic function.',
-        link: '/projects/notebooks/inverse-kinematic-approximations'
+        link: 'https://olivecasazza.github.io/notebooks/lab?path=SDSU-CS556-Workspace/a3/Assignment 3, Part 2.ipynb'
       }
     ]
   }
