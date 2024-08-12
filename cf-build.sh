38d26c653870dbbef5a03f0879178f88ce3a9df8
Olive Casazza <colanzio5@gmail.com>
2024-08-12 14:31:54 -0700

    cf build

--- cf-build.sh
+++ cf-build.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 # install rust and wasm-pack
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y ;
+curl https://sh.rustup.rs -sSf | sh -s -- -y ;
+. "$HOME/.cargo/env" ;
 curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh -s -- -y ;
 
 # build wasm package
