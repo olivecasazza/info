f5a7c1c814ea0b529002e6118cf32d4510e76681
Olive Casazza <colanzio5@gmail.com>
2024-08-12 14:18:40 -0700

    adds cf build script

---
+++ cf-build.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
+
+wasm-pack build ./wasm/flock --target web
