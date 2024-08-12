bc272e732dede2fa8a0158367de64ab472c46208
Olive Casazza <colanzio5@gmail.com>
2024-08-12 14:23:31 -0700

    fixes non-interactive install issue

--- cf-build.sh
+++ cf-build.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
+# install rust and wasm-pack
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh -s -- -y./
 
+# build wasm package
 wasm-pack build ./wasm/flock --target web
+
+# build nuxt package
+npm run build:pages
