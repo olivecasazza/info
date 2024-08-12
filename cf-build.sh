5317982d2ff67891f2bbb41caf28b97890840a0b
Olive Casazza <colanzio5@gmail.com>
2024-08-12 14:28:37 -0700

    cf-build changes

--- cf-build.sh
+++ cf-build.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 # install rust and wasm-pack
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh -s -- -y
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y ;
+curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh -s -- -y ;
 
 # build wasm package
-wasm-pack build ./wasm/flock --target web
+npm run build:wasm
 
 # build nuxt package
 npm run build:pages
