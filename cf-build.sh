f86bb176ce1b2b78f2deec0e1b6831a4bd03dfd2
Olive Casazza <colanzio5@gmail.com>
2024-08-12 14:41:51 -0700

    cf build

--- cf-build.sh
+++ cf-build.sh
@@ -5,6 +5,9 @@ curl https://sh.rustup.rs -sSf | sh -s -- -y ;
 . "$HOME/.cargo/env" ;
 curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh -s -- -y ;
 
+# install node dependencies
+npm install 
+
 # build wasm package
 npm run build:wasm
 
