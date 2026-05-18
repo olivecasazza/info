d31537c30848a4d3a2505261e0055cfd8105765b
Olive Casazza <olive@casazza.info>
2026-05-18 02:00:33 +0000

    fix(notebooks): wire legacy notebook routes to marimo static pages

--- pages/projects/notebooks/inverse-kinematic-approximations.vue
+++ pages/projects/notebooks/inverse-kinematic-approximations.vue
@@ -2,6 +2,6 @@
   <NotebookWrapper
     title="Inverse Kinematics Approximation"
     source-url="https://github.com/olivecasazza/SDSU-CS556-Workspace/blob/master/a3/Assignment%203%2C%20Part%202.ipynb"
-    notebook-path="https://olivecasazza.github.io/notebooks-refresh/notebooks/index.html?path=SDSU-CS556-Workspace%2Fa3%2FAssignment+3%2C+Part+2.ipynb"
+    notebook-path="https://olivecasazza.github.io/notebooks/notebooks/inverse-kinematic-approximations.html"
   />
 </template>
--- pages/projects/notebooks/kinematics.vue
+++ pages/projects/notebooks/kinematics.vue
@@ -2,6 +2,6 @@
   <NotebookWrapper
     title="Kinematics"
     source-url="https://github.com/olivecasazza/SDSU-CS556-Workspace/blob/master/a4/p4.ipynb"
-    notebook-path="https://olivecasazza.github.io/notebooks-refresh/notebooks/?path=SDSU-CS556-Workspace%2Fa4%2Fp4.ipynb"
+    notebook-path="https://olivecasazza.github.io/notebooks/notebooks/kinematics.html"
   />
 </template>
